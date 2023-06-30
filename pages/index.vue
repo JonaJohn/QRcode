fc6cafc7b9d55b874a180ae4d999c21180906460
jona <jona.john@gmx.de>
2023-06-30 19:31:01 +0200

    formatiert und 1 versuch

--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,40 @@
 <template>
+  <div class="grid grid-cols-2 divide-x">
     <div class="Mitte">
       <h3>QrCode:</h3>
-      <input style="margin: 10px;" v-model="value" type="text" >
-        <button class="bg-sky-500 hover:bg-sky-700 ..." @click="forceRerender()"> save changes</button>
-
-      <qrcode-vue v-if="renderComponent" :value="value" :size="size"  level="L" />
-
+      <input style="margin: 10px" v-model="value" type="text" />
+      <button class="bg-sky-500 hover:bg-sky-700 ..." @click="forceRerender()">
+        save changes
+      </button>
+      <qrcode-vue
+        v-if="renderComponent"
+        :value="value"
+        :size="size"
+        level="L"
+      />
     </div>
-  </template>
+    <div class="Mitte">
+      <h3>QrCode:</h3>
+      <input style="margin: 10px" v-model="value" type="text" />
+      <button class="bg-sky-500 hover:bg-sky-700 ..." @click="forceRerender()">
+        save changes
+      </button>
+      <qrcode-vue
+        v-if="renderComponent"
+        :value="value"
+        :size="size"
+        level="L"
+      />
+    </div>
+  </div>
+</template>
 
   <script setup>
-  import QrcodeVue from 'qrcode.vue'
-  import { nextTick, ref } from 'vue';
+import QrcodeVue from "qrcode.vue";
+import { nextTick, ref } from "vue";
 
-  const value = reactive('https://www.linkplotter.com/');
-  const size = reactive(400);
+const value = reactive("https://www.linkplotter.com/");
+const size = reactive(400);
 
 const renderComponent = ref(true);
 
@@ -22,18 +42,17 @@ const forceRerender = async () => {
   // Remove MyComponent from the DOM
   renderComponent.value = false;
 
-	// Wait for the change to get flushed to the DOM
-	await nextTick();
+  // Wait for the change to get flushed to the DOM
+  await nextTick();
 
-	// Add the component back in
+  // Add the component back in
   renderComponent.value = true;
 };
-
-  </script>
+</script>
 
   <style>
-.Mitte{
-    display: flex;
+.Mitte {
+  display: flex;
   flex-direction: column;
   padding: 10px;
 }
@@ -47,4 +66,4 @@ const forceRerender = async () => {
   font-size: 14px;
   cursor: pointer;
 }
-  </style>
+</style>
